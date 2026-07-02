97a9b498be16cd3264fa347b63b08cdbea2280d6
bkj0829 <bkj0829@gmail.com>
2026-07-02 13:43:09 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,880 @@
+window.ORDER_INBOUND_DATA = {
+  "title": "동대문 발주 & 입고 리스트",
+  "sourceFile": "동대문 발주&입고 리스트.xlsx",
+  "generatedAt": "2026-07-02 11:30:54 KST",
+  "summary": {
+    "totalOrdered": 79,
+    "totalReceived": 0,
+    "totalOutstanding": 79,
+    "rowCount": 45,
+    "vendorCount": 11,
+    "productCount": 16
+  },
+  "rows": [
+    {
+      "no": 1,
+      "excelRow": 5,
+      "vendor": "르누보",
+      "product": "플리츠스카프 BL",
+      "color": "아이보리",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 2,
+      "excelRow": 6,
+      "vendor": "르누보",
+      "product": "플리츠스카프 BL",
+      "color": "블랙",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 3,
+      "excelRow": 7,
+      "vendor": "르누보",
+      "product": "도브OPS",
+      "color": "연카키",
+      "size": "FREE",
+      "ordered": 5,
+      "received": 0,
+      "outstanding": 5,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        },
+        {
+          "round": 2,
+          "type": "order",
+          "qty": 3
+        }
+      ]
+    },
+    {
+      "no": 4,
+      "excelRow": 8,
+      "vendor": "르누보",
+      "product": "도브OPS",
+      "color": "차콜",
+      "size": "FREE",
+      "ordered": 5,
+      "received": 0,
+      "outstanding": 5,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        },
+        {
+          "round": 2,
+          "type": "order",
+          "qty": 3
+        }
+      ]
+    },
+    {
+      "no": 5,
+      "excelRow": 9,
+      "vendor": "로웰",
+      "product": "베로나OPS",
+      "color": "소라",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 6,
+      "excelRow": 10,
+      "vendor": "로웰",
+      "product": "베로나OPS",
+      "color": "차콜",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 7,
+      "excelRow": 11,
+      "vendor": "러브 앤 펄스",
+      "product": "선데이OPS",
+      "color": "화이트",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 8,
+      "excelRow": 12,
+      "vendor": "러브 앤 펄스",
+      "product": "선데이OPS",
+      "color": "블랙",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 9,
+      "excelRow": 13,
+      "vendor": "룸698",
+      "product": "베이 린넨 세트",
+      "color": "아이보리",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 10,
+      "excelRow": 14,
+      "vendor": "룸698",
+      "product": "베이 린넨 세트",
+      "color": "베이지",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 11,
+      "excelRow": 15,
+      "vendor": "리센스 홀가먼트",
+      "product": "나인 블럭 베스트",
+      "color": "아이보리",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 12,
+      "excelRow": 16,
+      "vendor": "리센스 홀가먼트",
+      "product": "나인 블럭 베스트",
+      "color": "브라운",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 13,
+      "excelRow": 17,
+      "vendor": "리센스 홀가먼트",
+      "product": "메쉬 펀칭 반팔니트",
+      "color": "아이보리",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 14,
+      "excelRow": 18,
+      "vendor": "리센스 홀가먼트",
+      "product": "메쉬 펀칭 반팔니트",
+      "color": "카키",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 15,
+      "excelRow": 19,
+      "vendor": "리센스 홀가먼트",
+      "product": "메쉬 펀칭 반팔니트",
+      "color": "블랙",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 16,
+      "excelRow": 20,
+      "vendor": "리센스 홀가먼트",
+      "product": "스윗 니트 나시",
+      "color": "아이보리",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 17,
+      "excelRow": 21,
+      "vendor": "리센스 홀가먼트",
+      "product": "스윗 니트 나시",
+      "color": "베이지",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 18,
+      "excelRow": 22,
+      "vendor": "리센스 홀가먼트",
+      "product": "스윗 니트 나시",
+      "color": "브라운(밤색)",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 19,
+      "excelRow": 23,
+      "vendor": "리센스 홀가먼트",
+      "product": "스윗 니트 나시",
+      "color": "블랙",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 20,
+      "excelRow": 24,
+      "vendor": "드마르",
+      "product": "키엘터크팬츠",
+      "color": "아이보리",
+      "size": "S",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 21,
+      "excelRow": 25,
+      "vendor": "드마르",
+      "product": "키엘터크팬츠",
+      "color": "아이보리",
+      "size": "M",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 22,
+      "excelRow": 26,
+      "vendor": "드마르",
+      "product": "키엘터크팬츠",
+      "color": "베이지",
+      "size": "S",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 23,
+      "excelRow": 27,
+      "vendor": "드마르",
+      "product": "키엘터크팬츠",
+      "color": "베이지",
+      "size": "M",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 24,
+      "excelRow": 28,
+      "vendor": "드마르",
+      "product": "키엘터크팬츠",
+      "color": "블랙",
+      "size": "S",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 25,
+      "excelRow": 29,
+      "vendor": "드마르",
+      "product": "키엘터크팬츠",
+      "color": "블랙",
+      "size": "M",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 26,
+      "excelRow": 30,
+      "vendor": "드마르",
+      "product": "슈가데님팬츠",
+      "color": "중청",
+      "size": "S",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 27,
+      "excelRow": 31,
+      "vendor": "드마르",
+      "product": "슈가데님팬츠",
+      "color": "중청",
+      "size": "M",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 28,
+      "excelRow": 32,
+      "vendor": "드마르",
+      "product": "슈가데님팬츠",
+      "color": "중청",
+      "size": "L",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 29,
+      "excelRow": 33,
+      "vendor": "드마르",
+      "product": "슈가데님팬츠",
+      "color": "진청",
+      "size": "S",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 30,
+      "excelRow": 34,
+      "vendor": "드마르",
+      "product": "슈가데님팬츠",
+      "color": "진청",
+      "size": "M",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 31,
+      "excelRow": 35,
+      "vendor": "드마르",
+      "product": "슈가데님팬츠",
+      "color": "진청",
+      "size": "L",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 32,
+      "excelRow": 36,
+      "vendor": "헤이즈",
+      "product": "포스트BL",
+      "color": "진베이지",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 33,
+      "excelRow": 37,
+      "vendor": "헤이즈",
+      "product": "포스트BL",
+      "color": "블랙",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 34,
+      "excelRow": 38,
+      "vendor": "헤이즈",
+      "product": "포스트밴딩PT",
+      "color": "진베이지",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 35,
+      "excelRow": 39,
+      "vendor": "헤이즈",
+      "product": "포스트밴딩PT",
+      "color": "블랙",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 36,
+      "excelRow": 40,
+      "vendor": "오르디",
+      "product": "린넨투피스셋업",
+      "color": "그레이",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 37,
+      "excelRow": 41,
+      "vendor": "비바글램",
+      "product": "린넨슬립",
+      "color": "아이보리",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 38,
+      "excelRow": 42,
+      "vendor": "비바글램",
+      "product": "린넨슬립",
+      "color": "블랙",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 39,
+      "excelRow": 43,
+      "vendor": "비바글램",
+      "product": "린넨슬립",
+      "color": "브라운",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 40,
+      "excelRow": 44,
+      "vendor": "비바글램",
+      "product": "린넨슬립",
+      "color": "오트밀",
+      "size": "FREE",
+      "ordered": 2,
+      "received": 0,
+      "outstanding": 2,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 2
+        }
+      ]
+    },
+    {
+      "no": 41,
+      "excelRow": 45,
+      "vendor": "센드",
+      "product": "밴딩카프리팬츠",
+      "color": "블랙",
+      "size": "FREE",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 42,
+      "excelRow": 46,
+      "vendor": "센드",
+      "product": "밴딩카프리팬츠",
+      "color": "아이보리",
+      "size": "FREE",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 43,
+      "excelRow": 47,
+      "vendor": "레어썸",
+      "product": "321 아이스 와이드 팬츠",
+      "color": "연청(아이스)",
+      "size": "S",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 44,
+      "excelRow": 48,
+      "vendor": "레어썸",
+      "product": "321 아이스 와이드 팬츠",
+      "color": "연청(아이스)",
+      "size": "M",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    },
+    {
+      "no": 45,
+      "excelRow": 49,
+      "vendor": "레어썸",
+      "product": "321 아이스 와이드 팬츠",
+      "color": "연청(아이스)",
+      "size": "L",
+      "ordered": 1,
+      "received": 0,
+      "outstanding": 1,
+      "note": "",
+      "movements": [
+        {
+          "round": 1,
+          "type": "order",
+          "qty": 1
+        }
+      ]
+    }
+  ]
+};
