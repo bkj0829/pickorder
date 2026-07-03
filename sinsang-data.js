80436dda0e11268d27e917681a65f2c9d137d364
bkj0829 <bkj0829@gmail.com>
2026-07-03 16:16:55 +0900

    Add SINSANG_ARRIVAL_SAMPLE data structure

---
+++ sinsang-data.js
@@ -0,0 +1,656 @@
+window.SINSANG_ARRIVAL_SAMPLE = {
+  "sourceUrl": "https://sinsangmarket.kr/myPage/orderList/sinsang/3616456",
+  "orderNo": "202607013616456",
+  "orderDate": "2026-07-01 10:04",
+  "generatedAt": "2026. 7. 3. 14시 28분 44초",
+  "products": [
+    {
+      "store": "",
+      "product": "26ss)린넨슬립(4컬러) 린넨나시 끈나시 린넨혼방",
+      "price": "₩9,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/3894/99729113/177244837900662897_643006775.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "아이보리 Free ₩9,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "미송 입고완료 (20260702)",
+          "shipInfo": "내용없음",
+          "optionTotal": "₩18,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "블랙 Free ₩9,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩18,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "브라운 Free ₩9,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "미송 입고완료 (20260702)",
+          "shipInfo": "내용없음",
+          "optionTotal": "₩18,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "오트밀 Free ₩9,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩18,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 8,
+      "totalArrived": 8,
+      "totalRefund": 0,
+      "partial": true,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "린넨투피스셋업(26ss 여름신상 세트상품 시원한 찰랑원단 블라우스 팬츠",
+      "price": "₩56,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/14936/100310060/178059688300835980_1931118936.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "그레이 Free ₩56,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩112,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 2,
+      "totalArrived": 2,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "26ss)포스트BL _ 고급미 하객룩, 세트상품, 뱃살커버, 시원한네크",
+      "price": "₩33,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/16569/100283566/178023466300763763_1657928427.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "진베이지 Free ₩33,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩66,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "블랙 Free ₩33,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩66,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 4,
+      "totalArrived": 4,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "26ss)포스트밴딩PT _ 찰랑찰랑 딱떨어지는 밴드바지, 셋업, 모임룩",
+      "price": "₩36,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/16569/100283656/17807696240074837_669474359.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "진베이지 Free ₩36,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩72,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "블랙 Free ₩36,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩72,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 4,
+      "totalArrived": 4,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "밴딩카프리팬츠 (26ss 여름 신상 7부 반바지 프리 하객룩 버뮤다)",
+      "price": "₩18,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/18209/100035767/177661073400415463_1690504319.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "블랙 Free ₩18,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩18,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "아이보리 Free ₩18,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩18,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 2,
+      "totalArrived": 2,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "(린넨코튼)나인 블럭 펀칭 버튼 니트조끼 베스트 나시 뷔스티에",
+      "price": "₩12,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/15198/98571833/175858615000966722_1265993374.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "아이보리 Free ₩12,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩24,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "브라운 Free ₩12,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩24,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 4,
+      "totalArrived": 4,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "[린넨코튼]메쉬 카라 펀칭스카시 그물 반팔 니트티(네트)",
+      "price": "₩15,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/18405/96066002/171772124200729068_43300366.png&w=200&h=200",
+      "options": [
+        {
+          "option": "아이보리 Free ₩15,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩30,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "카키 Free ₩15,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩30,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "검정 Free ₩15,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩30,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 6,
+      "totalArrived": 6,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "[워셔블안티필링]스윗 니트 나시 뷔스티에 베스트[세탁기,건조기 사용가능]",
+      "price": "₩9,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/18405/98313609/174951653000344355_1698844951.png&w=200&h=200",
+      "options": [
+        {
+          "option": "아이보리 Free ₩9,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩18,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "베이지 Free ₩9,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩18,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "밤색 Free ₩9,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩18,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "블랙 Free ₩9,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩18,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 8,
+      "totalArrived": 8,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "다리가날씬해보이는)키엘터크팬츠 롤업반바지 면바지 핀턱반바지",
+      "price": "₩39,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/18645/100309558/178059723300376259_971997001.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "베이지 S ₩39,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩39,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "베이지 M ₩39,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩39,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "블랙 S ₩39,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩39,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "블랙 M ₩39,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩39,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "아이보리 S ₩39,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩39,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "아이보리 M ₩39,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩39,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 6,
+      "totalArrived": 6,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "골반에걸텨입는)슈가데님팬츠 청반바지 핀턱데님 여름반바지 드마르",
+      "price": "₩31,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/18645/100320243/178085635100839770_1965127032.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "중청 S ₩31,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩31,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "중청 M ₩31,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩31,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "중청 L ₩31,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩31,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "진청 S ₩31,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩31,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "진청 M ₩31,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩31,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "진청 L ₩31,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩31,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 6,
+      "totalArrived": 6,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "신상)선데이OPS (허리끈디테일 절개 롱 원피스 퍼프소매 반팔원피스 시원",
+      "price": "₩32,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/21179/100364515/178157184500772494_1346891760.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "화이트 Free ₩32,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩64,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "블랙 Free ₩32,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩64,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 4,
+      "totalArrived": 4,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "26SS 신상 데님 워싱 나시 롱원피스 - ( 베로나 원피스 )",
+      "price": "₩28,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/22921/97958520/174358088700863941_507093624.png&w=200&h=200",
+      "options": [
+        {
+          "option": "차콜 Free ₩28,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩56,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 2,
+      "totalArrived": 2,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "플리츠스카프bl(26ss,신상,스카프세트,플리츠",
+      "price": "₩19,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/25258/100119779/17777724740081997_385759252.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "아이보리 Free ₩19,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "미송 입고완료 (20260702)",
+          "shipInfo": "내용없음",
+          "optionTotal": "₩38,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "블랙 Free ₩19,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료 (입고일 확인불가 도매 확인 후 재주문 요망)",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩38,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 4,
+      "totalArrived": 4,
+      "totalRefund": 0,
+      "partial": true,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "도브ops(26ss,리버시블,신상,와끼포켓",
+      "price": "₩37,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/25258/99891277/177474294900536286_1851707783.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "차콜 Free ₩37,000",
+          "qtyStatus": "주문 • 2 입고 • 1 환불 • 0",
+          "siteStatus": "미송 입고지연 (2026-07-06 추가사입예정 )",
+          "shipInfo": "내용없음",
+          "optionTotal": "₩74,000",
+          "order": 2,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "연카키 Free ₩37,000",
+          "qtyStatus": "주문 • 2 입고 • 1 환불 • 0",
+          "siteStatus": "미송 입고지연 (2026-07-06 추가사입예정 )",
+          "shipInfo": "내용없음",
+          "optionTotal": "₩74,000",
+          "order": 2,
+          "arrived": 1,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 4,
+      "totalArrived": 2,
+      "totalRefund": 0,
+      "partial": true,
+      "delayed": true
+    },
+    {
+      "store": "",
+      "product": "베이 린넨 세트 [반팔 블라우스 밴딩 바지 긴바지 여름]",
+      "price": "₩50,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/26643/99942501/177540075100897582_1280775028.png&w=200&h=200",
+      "options": [
+        {
+          "option": "아이보리 Free ₩50,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩100,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        },
+        {
+          "option": "베이지 Free ₩50,000",
+          "qtyStatus": "주문 • 2 입고 • 2 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩100,000",
+          "order": 2,
+          "arrived": 2,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 4,
+      "totalArrived": 4,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    },
+    {
+      "store": "",
+      "product": "321아이스소프트와이드팬츠(레어썸,여름신상,청바지,,데님,여름바지,워싱)",
+      "price": "₩21,000",
+      "image": "https://v4.img.sinsang.market/?f=https://image-cache.sinsang.market/images/29557/100010964/177623222900855829_1070997551.jpg&w=200&h=200",
+      "options": [
+        {
+          "option": "아이스 S ₩21,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩21,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "아이스 M ₩21,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩21,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        },
+        {
+          "option": "아이스 L ₩21,000",
+          "qtyStatus": "주문 • 1 입고 • 1 환불 • 0",
+          "siteStatus": "모든수량 입고완료",
+          "shipInfo": "배송완료",
+          "optionTotal": "₩21,000",
+          "order": 1,
+          "arrived": 1,
+          "refund": 0
+        }
+      ],
+      "totalOrder": 3,
+      "totalArrived": 3,
+      "totalRefund": 0,
+      "partial": false,
+      "delayed": false
+    }
+  ]
+};
